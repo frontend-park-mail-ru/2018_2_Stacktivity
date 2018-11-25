cd76431d68b751562aa2e2e4e6ae7348a360575c
ConstaConst <mitrkos@mail.ru>
2018-11-25 17:12:01 +0300

    Chat conversation

--- dev/js/views/ChatView.js
+++ dev/js/views/ChatView.js
@@ -62,15 +62,96 @@ export default class ChatView extends BaseView {
         this.viewSection.innerHTML += `
     <div class="chat">
     	<div class="chat__talkboard">
-        
         </div>
        
-        <div class="chat__content">
-        
+        <div class="chat__conversation conversation">
+            <div class="conversation__messages messages">
+            
+                <div class="messages__messagesunion messagesunion">
+                    
+                    <div class="messagesunion__messageblock messageblock">
+                        
+                        <div class="messageblock__author author">
+                            <div class="autor__unionauthordata unionauthordata">
+                                <div class="unionauthordata__nickname">
+                                        nickname
+                                </div>        
+                                <div class="unionauthordata__circle">
+                                </div>
+                            </div>
+                        </div>  
+                            
+                        <div class="messageblock__messagedata messagedata">
+                            <div class="messagedata__dataunion dataunion">
+                                <div class="dataunion__messagetext">
+Указывает, что элемент абсолютно позиционирован, при этом другие элементы отображаются на веб-странице словно абсолютно позиционированного элемента и нет. Положение элемента задается свойствами left, top, right и bottom, также на положение влияет значение свойства position родительского элемента. Так, если у родителя значение position установлено как static или родителя нет, то отсчет координат ведется от края окна браузера. Если у родителя значение position задано как fixed, relative или absolute, то отсчет координат ведется от края родительского элемента.
+                                </div>
+                                <hr>
+                                <div class="dataunion__messagedate">
+                                    21.21.2112 12:12
+                                </div>
+                            </div>    
+                        </div>  
+                    </div>
+                    
+                    <div class="messagesunion__messageblock messageblock">
+                        
+                        <div class="messageblock__author author">
+                            <div class="autor__unionauthordata unionauthordata">
+                                <div class="unionauthordata__nickname">
+                                        nickname
+                                </div>        
+                                <div class="unionauthordata__circle">
+                                </div>
+                            </div>
+                        </div>  
+                            
+                        <div class="messageblock__messagedata messagedata">
+                            <div class="messagedata__dataunion dataunion">
+                                <div class="dataunion__messagetext">
+Указывает, что элемент абсолютно позиционирован, при этом другие элементы отображаются на веб-странице словно абсолютно позиционированного элемента и нет. Положение элемента задается свойствами left, top, right и bottom, также на положение влияет значение свойства position родительского элемента. Так, если у родителя значение position установлено как static или родителя нет, то отсчет координат ведется от края окна браузера. Если у родителя значение position задано как fixed, relative или absolute, то отсчет координат ведется от края родительского элемента.
+                                </div>
+                                <hr>
+                                <div class="dataunion__messagedate">
+                                    21.21.2112 12:12
+                                </div>
+                            </div>    
+                        </div>  
+                    </div>
+                    
+                    <div class="messagesunion__messageblock messageblock">
+                        
+                        <div class="messageblock__author author">
+                            <div class="autor__unionauthordata unionauthordata">
+                                <div class="unionauthordata__nickname">
+                                        nickname
+                                </div>        
+                                <div class="unionauthordata__circle">
+                                </div>
+                            </div>
+                        </div>
+                            
+                        <div class="messageblock__messagedata messagedata">
+                            <div class="messagedata__dataunion dataunion">
+                                <div class="dataunion__messagetext">
+Указывает,
+                                </div>
+                                <hr>
+                                <div class="dataunion__messagedate">
+                                    21.21.2112 12:12
+                                </div>
+                            </div>    
+                        </div>  
+                    </div>
+                    
+                </div>
+            </div>
+            <div class="conversation__messageinput messageinput">
+            </div>
         </div>
         
-        <div class="chat__userboard">
         
+        <div class="chat__userboard">
         </div>
     </div>
 `
