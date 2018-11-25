5a5a595ef964192120e4d958b08b6b9eeb0acf9f
Sergey Lomachev <anoscillator@gmail.com>
2018-11-25 19:58:09 +0300

    config

--- dev/js/config.js
+++ dev/js/config.js
@@ -1,8 +1,8 @@
 export const basePath = "http://localhost:3001";
 export const backWSPath = "ws://localhost:3001";
 
-export const WSPath = backWSPath + "/game/singleplayer";
-export const chatPath = backWSPath + "/chat";
+export const WSPath = "ws://localhost:8083/game/singleplayer";
+export const chatPath = "ws://localhost:8084/chat";
 
 export const infoLevel = 3; // 3 - all, 2 - warn and info, 1 - info, 0 - none
 export const leaderboardLimit = 5;
